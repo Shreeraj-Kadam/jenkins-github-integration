91f121f8b63257e010d1f8b693d48f8b69687940
Shreeraj <kadamshreeraj1999@gmail.com>
2026-06-17 16:24:44 +0530

    Added new dummy test file

---
+++ mavenConfigProj/src/test/java/tests/NewDummyTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class NewDummyTest {
+	
+	@Test
+	public void dummyTest() {
+		
+		System.out.println("Dummy Test executed ....");
+		
+	}
+	
+}
